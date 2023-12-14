22e5fb8acc5ebb2095989ca84ecf36f233c05d1d
Facundo A. Pestana <136341998+FacuPestana@users.noreply.github.com>
2023-12-14 20:49:53 -0300

    Final Version
    Puse para cambiar la imagen y solucione errores del js.

---
+++ script.js
@@ -0,0 +1,82 @@
+let img1play3 = document.querySelector('#play3img1')
+let boton = document.querySelector('#Play3')
+let imagenCambiada = false;
+boton.onclick = function(){
+    if (imagenCambiada == false){
+        img1play3.src = 'https://th.bing.com/th/id/R.0977475e0804deb31ac64ed6cc94ae91?rik=i8K3Rp3NvJGxjg&pid=ImgRaw&r=0'
+        imagenCambiada = true
+    } else {
+        img1play3.src = 'https://i5.walmartimages.com/seo/Restored-Sony-PlayStation-3-PS3-Super-Slim-System-500GB-Refurbished_7a81a26c-2144-4d94-bf44-9a118f73722e.34cf88838325597102df09abfbf93ab8.jpeg'
+   
+    imagenCambiada = false
+    
+    }
+}
+
+let img1nintendo = document.querySelector('#nintendoimg1')
+let boton2 = document.querySelector('#nintendobtn')
+let imagenCambiada2 = false;
+boton2.onclick = function(){
+    if (imagenCambiada2 == false){
+        img1nintendo.src = 'https://th.bing.com/th/id/OIP.h6mUGY6quvM1iIHIXqyuVwHaEO?w=302&h=180&c=7&r=0&o=5&pid=1.7'
+        imagenCambiada2 = true
+    } else {
+        img1nintendo.src = 'https://th.bing.com/th/id/OIP.LkBNHJMLIBmi0RzN6WLL1wHaFD?w=192&h=181&c=7&r=0&o=5&pid=1.7'
+   
+    imagenCambiada2 = false
+    
+    }
+}
+
+let img1ps4 = document.querySelector('#play4img1')
+let boton3 = document.querySelector('#ps4btn')
+let imagenCambiada3 = false;
+boton3.onclick = function(){
+    if (imagenCambiada3 == false){
+        img1ps4.src = 'https://th.bing.com/th/id/OIP.0w_gF4bcftv0FD-9ph6mLgHaHa?w=167&h=180&c=7&r=0&o=5&pid=1.7'
+        imagenCambiada3 = true
+    } else {
+        img1ps4.src = 'https://th.bing.com/th/id/OIP.0mveA4IixS3C_UC2ReOMAgHaG4?w=182&h=180&c=7&r=0&o=5&pid=1.7'
+   
+    imagenCambiada3 = false
+    
+    }
+}
+
+
+let img1ps5 = document.querySelector('#PS5img1')
+let boton4 = document.querySelector('#ps5btn')
+let imagenCambiada4 = false;
+boton4.onclick = function(){
+    if (imagenCambiada4 == false){
+        img1ps5.src = 'https://th.bing.com/th/id/R.a0b7059e2aadd4de61e5f381a1813373?rik=aoJL0co6c%2f428w&pid=ImgRaw&r=0'
+        imagenCambiada4 = true
+    } else {
+        img1ps5.src = 'https://cdn.mos.cms.futurecdn.net/7Qv4q73m9Nif9CpxtfVWs6.jpg'
+   
+    imagenCambiada4 = false
+    
+    }
+}
+//Xbox 360
+
+let img1xbox = document.querySelector('#xbox1')
+let boton5 = document.querySelector('#btnxbox')
+let imagenCambiada5 = false;
+boton5.onclick = function(){
+    if (imagenCambiada5 == false){
+        img1xbox.src = 'https://pricespy-75b8.kxcdn.com/product/standard/280/1666255.jpg'
+        imagenCambiada5 = true
+    } else {
+        img1xbox.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7xpD3SGpt1C25e8u_XgRgNnjiHGpRRoJAgxXmxr2NABHUcs_5ZX3u_eStcGPz2JKaf7E&usqp=CAU'
+   
+    imagenCambiada5 = false
+    
+    }
+}
+
+
+alert('Bienvenido a esta pagina sobre las "Mejores Consolas De La Historia"')
+
+let pregunta = prompt('¿Cual es tu consola Favorita?')
+alert('Genial, ahora te presento mi top')
